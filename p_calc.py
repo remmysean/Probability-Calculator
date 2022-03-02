84dc2ae9b64cd6199d90f7a13716c78ea5582222
Remission Pasurai <38808548+remmysean@users.noreply.github.com>
2022-03-02 10:34:57 +0200

    Update p_calc.py

--- p_calc.py
+++ p_calc.py
@@ -1,22 +1,37 @@
-import copy, random
-
+import copy
+import random
 
 
 class Hat:
-    def __init__(self, **kwargs):
-        self.contents = [k for k, v in kwargs.items() for _ in range(v)]
+  def __init__(self,**group):
+    self.contents=[]
+    for key,value in group.items():
+        for itr in range(value):
+          self.contents.append(key)
+  
+  #  get random n number of hats from contents
+def draw(self, amount):
+    if amount > len(self.contents):
+      return self.contents
+    draw_list = []
+    for _ in range(amount):
+      choice = random.randrange(len(self.contents))
+       draw_list.append(self.contents.pop(choice))
+    return  draw_list
 
-    def draw(self, n):
-        n = min(n, len(self.contents))
-        return [self.contents.pop(random.randrange(len(self.contents))) for _ in range(n)]
 
-
+  
+  #  probability
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    m = 0
+    final_count=0
     for _ in range(num_experiments):
-        another_hat = copy.deepcopy(hat)
-        balls_drawn = another_hat.draw(num_balls_drawn)
-        balls_req = sum([1 for k, v in expected_balls.items() if balls_drawn.count(k) >= v])
-        m += 1 if balls_req == len(expected_balls) else 0
-
-    return m / num_experiments
+      copyhat = copy.deepcopy(hat)
+      temp_list = copyhat.draw(num_balls_drawn)
+      success = True
+      for key,value in expected_balls.items():
+        if temp_list.count(key) < value:
+          success = False
+          break
+      if success:
+        final_count+=1
+    return final_count/num_experiments
